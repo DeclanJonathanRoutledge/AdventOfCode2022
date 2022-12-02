f063acbd5d6bb8d262acfdd16d5ef3ed21f8fd55
Declan Routledge <90246178+DeclanJonathanRoutledge@users.noreply.github.com>
2022-12-02 13:57:51 +0000

    DayTwo of adventofCode: refactor adding small check on answers provided (#2)

--- RockPaperScissors.scala
+++ RockPaperScissors.scala
@@ -12,13 +12,13 @@ import scala.io.Source
 case class guide(enemyChoice:String, yourChoice :String)
 case class roundScore(round:Int, letterScore: Int, totalForRound: Int)
 
-val scissors = 3
-val paper = 2
-val rock = 1
+val scissorsScore = 3
+val paperScore = 2
+val rockScore = 1
 
-val lose = 0
-val draw = 3
-val win = 6
+val loseScore = 0
+val drawScore = 3
+val winScore = 6
 
 object RockPaperScissors{
   def scoreProccesor(data:Iterator[String]):Seq[guide] ={
@@ -38,43 +38,40 @@ object RockPaperScissors{
     case "C" => enemyCScissors(round)
   }
   def enemyARock(round:guide): roundScore=round.yourChoice match {
-    case "X" => roundScore(3,1,4)//rock
-    case "Y" => roundScore(6,2,8)//paper
-    case "Z" => roundScore(0,3,3)//SCISSORS
+    case "X" => roundScore(drawScore,rockScore,drawScore + rockScore)//rock
+    case "Y" => roundScore(winScore,paperScore,winScore + paperScore)//paper
+    case "Z" => roundScore(loseScore,scissorsScore,loseScore + scissorsScore)//SCISSORS
   }
   def enemyBPaper(round:guide): roundScore=round.yourChoice match {
-    case "X" => roundScore(0,1,1)//rock
-    case "Y" => roundScore(3,2,5)//paper
-    case "Z" => roundScore(6,3,9)//SCISSORS
+    case "X" => roundScore(loseScore,rockScore,loseScore + rockScore)//rock
+    case "Y" => roundScore(drawScore,paperScore,drawScore + paperScore)//paper
+    case "Z" => roundScore(winScore,scissorsScore,winScore + scissorsScore)//SCISSORS
   }
   def enemyCScissors(round:guide): roundScore=round.yourChoice match {
-    case "X" => roundScore(6,1,7)//rock
-    case "Y" => roundScore(0,2,2)//paper
-    case "Z" => roundScore(3,3,6)//SCISSORS
+    case "X" => roundScore(winScore,rockScore,winScore + rockScore)//rock
+    case "Y" => roundScore(loseScore,paperScore,loseScore + paperScore)//paper
+    case "Z" => roundScore(drawScore,scissorsScore,drawScore + scissorsScore)//SCISSORS
   }
 
   def translatorPt2(round:guide):roundScore = round.yourChoice match {
     case "X" => losingGame(round)
     case "Y" => drawGame(round)
     case "Z" => winningGame(round)
-    // A||X ROCK 1  pt2 x =lose
-    // B||Y PAPER 2 pt2 y = draw
-    // C||Z SCISSORS 3 pt z = win
   }
   def losingGame(round:guide): roundScore=round.enemyChoice match {
-    case "A" => roundScore(lose,scissors,lose + scissors)//rock -- SCISSORS
-    case "B" => roundScore(lose,rock,lose +rock)//paper -- rock
-    case "C" => roundScore(lose,paper,lose +paper)//SCISSORS -- paper
+    case "A" => roundScore(loseScore,scissorsScore,loseScore + scissorsScore)//rock -- SCISSORS
+    case "B" => roundScore(loseScore,rockScore,loseScore +rockScore)//paper -- rock
+    case "C" => roundScore(loseScore,paperScore,loseScore +paperScore)//SCISSORS -- paper
   }
   def drawGame(round:guide): roundScore=round.enemyChoice match {
-    case "A" => roundScore(draw,rock,draw + rock)//rock -- rock
-    case "B" => roundScore(draw,paper,draw +paper)//paper -- paper
-    case "C" => roundScore(draw,scissors,draw +scissors)//SCISSORS -- SCISSORS
+    case "A" => roundScore(drawScore,rockScore,drawScore + rockScore)//rock -- rock
+    case "B" => roundScore(drawScore,paperScore,drawScore +paperScore)//paper -- paper
+    case "C" => roundScore(drawScore,scissorsScore,drawScore +scissorsScore)//SCISSORS -- SCISSORS
   }
   def winningGame(round:guide): roundScore=round.enemyChoice match {
-    case "A" => roundScore(win,paper,win + paper)//rock -- paper
-    case "B" => roundScore(win,scissors,win +scissors)//paper -- SCISSORS
-    case "C" => roundScore(win,rock,win +rock)//SCISSORS -- rock
+    case "A" => roundScore(winScore,paperScore,winScore + paperScore)//rock -- paper
+    case "B" => roundScore(winScore,scissorsScore,winScore +scissorsScore)//paper -- SCISSORS
+    case "C" => roundScore(winScore,rockScore,winScore +rockScore)//SCISSORS -- rock
   }
 
   @tailrec
@@ -89,8 +86,9 @@ val rounds = scoreProccesor(rawData.getLines())
 
 //---answer to part 1
 println("total score determined by given moves: "+ totalScoreAdder(umpire(rounds, translatorPt1)))
+println("match expected output: "+ (totalScoreAdder(umpire(rounds, translatorPt1)) equals 14297))
 ////---answer to part 2
 println("total score determined by given outcome: "+ totalScoreAdder(umpire(rounds, translatorPt2)))
-
+println("match expected output: "+  (totalScoreAdder(umpire(rounds, translatorPt2)) equals 10498))
 
 rawData.close()
